f2b8130eb065557f6b208b2326ecf1e1c603ebd7
anas <anas.osm@outlook.com>
2023-10-27 01:49:58 -0400

    bind data

--- api_post_data.php
+++ api_post_data.php
@@ -1,11 +1,41 @@
 <?php
 
-$data = $_POST['data'];
-$location = $_POST['location'];
-$myFile = "test.txt";
-$fh = fopen($myFile, 'w') or die("can't open file");
-fwrite($fh, $data);
-fwrite($fh, $location);
-fclose($fh);
+// Établissez la connexion à la base de données
+$host = "localhost";
+$user = "anaso518_Pareildoliajl12h3109";
+$pass = "-CPgIYHI-(Kr";
+$db = "anaso518_Pareildolia_i1uy2n3";
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+
+if ($mysqli->connect_error) {
+    die("La connexion à la base de données a échoué : " . $mysqli->connect_error);
+}
+
+// Votre données JSON brut
+// $jsonData = '{"nom": "John", "age": 30, "ville": "Paris"}';
+
+// Préparez la requête SQL avec un paramètre
+// $query = "INSERT INTO votre_table (ID, DateCreation, IP, UserAgent, DataJson) VALUES (?, ?, ?, ?, ?)";
+$query = "INSERT INTO votre_table (DataJson) VALUES (?)";
+$stmt = $mysqli->prepare($query);
+
+if ($stmt === false) {
+    die("Erreur de préparation de la requête : " . $mysqli->error);
+}
+
+// Liez le paramètre
+$stmt->bind_param("s", $jsonData);
+
+// Exécutez la requête
+if ($stmt->execute()) {
+    echo "Données JSON insérées avec succès.";
+} else {
+    echo "Erreur lors de l'insertion des données JSON : " . $stmt->error;
+}
+
+// Fermez la connexion
+$stmt->close();
+$mysqli->close();
 
 ?>
